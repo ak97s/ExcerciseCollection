f596c54ec10c66953882e0425e9cec534177ec44
Syeh Abidin Kobar <syeh.ak@gmail.com>
2018-11-23 21:04:46 +0700

    tambah data class

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,13 @@
-   fun main(args: Array<String>){
-        println(args)
-        println("hello world")
+data class Conversation(val name:String,
+                   val lastMsg: String,
+                   val unreadMsg: Int,
+                   val time: String){
+    fun getNameAndMessage(): String{
+        return "Name = $name, last message = $lastMsg"
     }
+}
+fun main(args: Array<String>) {
+    val conversationJoko = Conversation("Joko","Hai",30,"19.45")
+    println(conversationJoko)
+    println(conversationJoko.getNameAndMessage())
+}
